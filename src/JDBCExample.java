96b83729ac6b93fef9364133c01b83de3398113c
dean <nifr@kea.dk>
2020-03-27 11:30:58 +0100

    Commit

--- src/JDBCExample.java
+++ src/JDBCExample.java
@@ -3,7 +3,7 @@ import java.sql.*;
 public class JDBCExample {
     public static void main(String[] args) throws SQLException {
         //Create connection
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/","","");
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + "[DB_NAME]","[YOUR USER]","[YOUR PASSWORD");
         //Create Statement
         Statement statement = conn.createStatement();
         //Execute Query
