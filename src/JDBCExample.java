4b278b22eb48d4d9499855505e23f686c0b2f0a5
dean <nifr@kea.dk>
2020-03-27 11:27:15 +0100

    First commit

---
+++ src/JDBCExample.java
@@ -0,0 +1,15 @@
+import java.sql.*;
+
+public class JDBCExample {
+    public static void main(String[] args) throws SQLException {
+        //Create connection
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/","","");
+        //Create Statement
+        Statement statement = conn.createStatement();
+        //Execute Query
+        ResultSet rs = statement.executeQuery("select * from emp");
+        //Print the set
+        while(rs.next()){
+        }
+    }
+}
